fae54038e5ce12eccb44e7bb9fef473b3429d669
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-02-08 03:25:46 +0530

    added some Icon component used in Index.js

---
+++ modelfrontend/src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import { MenuCloseIcon } from "./MenuCloseIcon";
+import { MenuOpenIcon } from "./MenuOpenIcon";
+import { BellIcon } from "./BellIcon";
+import { DashboardIcon } from "./DashboardIcon";
+
+export { BellIcon, MenuCloseIcon , MenuOpenIcon , DashboardIcon};
