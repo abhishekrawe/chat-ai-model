96456cd3be7ac8108312fc9c2304a2a8eb9d5ad6
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-02-08 03:50:24 +0530

    used react-router-dom added layout in App.js

--- modelfrontend/src/App.js
+++ modelfrontend/src/App.js
@@ -1,33 +1,15 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import ChatSection from "./components/ChatSection";
 
-function App() {
-  const [question, setQuestion] = useState("");
-  const [response, setResponse] = useState("");
-
-  const handleAsk = async () => {
-    try {
-      const res = await axios.post("/", { question });
-      setResponse(res.data.response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+export default function App() {
   return (
-    <div>
-      <h1>Ask a Question</h1>
-      <input
-        type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-      <button onClick={handleAsk}>Ask</button>
-      <p>Response: {response}</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path='/' element={<ChatSection />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
-
