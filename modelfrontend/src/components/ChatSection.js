08a39da7cc9215caf73f8a2334ee5c8e633650bd
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-02-08 12:17:54 +0530

    Added a Static design to StaticSuggestion

--- modelfrontend/src/components/ChatSection.js
+++ modelfrontend/src/components/ChatSection.js
@@ -1,32 +1,72 @@
 import React, { useState } from "react";
 import axios from "axios";
+import StaticSugestion from "./StaticSugestion";
 
 function ChatSection() {
   const [question, setQuestion] = useState("");
-  const [response, setResponse] = useState("");
+  const [conversation, setConversation] = useState([]);
 
   const handleAsk = async () => {
     try {
       const res = await axios.post("/", { question });
-      setResponse(res.data.response);
+      const newConversation = [
+        ...conversation,
+        { question, response: res.data.response },
+      ];
+      setConversation(newConversation);
+      setQuestion("");
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-      <button onClick={handleAsk}>Ask</button>
-      <p>Response: {response}</p>
+    <div className="flex flex-col">
+       <StaticSugestion/>
+      <div className="flex-grow p-4">
+        {conversation.map((item, index) => (
+          <div key={index}>
+            <div className="flex py-5">
+              <img
+                src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+                className="h-12 w-12 rounded-full"
+                alt="user"
+              />
+              <p className="bg-gray-200 rounded-lg p-2 mb-2">
+                <span className="font-bold text-lg">You : </span>
+                {item.question}
+              </p>
+            </div>
+            <div className="flex py-5">
+              <img
+                src="https://www.soulhq.ai/assets/soul-ui-logo.webp"
+                className="h-12 w-12 rounded-full"
+                alt="user"
+              />
+              <p className="bg-blue-200 rounded-lg p-2">
+                <span className="font-bold text-lg">SoulAI : </span>
+                {item.response}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center p-4">
+        <input
+          type="text"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          placeholder="Chat with AI model powerd by soul AI..."
+          className="flex-grow rounded-lg p-4 mr-4 shadow-md border-gray-text border "
+        />
+        <button
+          onClick={handleAsk}
+          className="px-4 py-4 rounded-lg bg-blue-10 text-white-10">
+          Ask
+        </button>
+      </div>
     </div>
   );
 }
 
 export default ChatSection;
-
-
---
+++ modelfrontend/src/components/StaticSugestion.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function StaticSugestion() {
+  return (
+    <>
+      <div class="flex flex-col items-center justify-center p-5 mb-28">
+        <img
+          src="https://www.soulhq.ai/assets/soul-ui-logo.webp"
+          alt="logo"
+          class="w-12 h-12 rounded-md"
+        />
+        <h1 class="text-center font-semibold text-xl mt-2">
+          How can I help you today?
+        </h1>
+      </div>
+      <div className="flex flex-wrap justify-between">
+        <div className="w-full md:w-1/2 lg:w-1/2 mb-6 md:mb-0 p-3">
+          <div className="bg-white-10 rounded-lg shadow-md p-4">
+            <p className="font-semibold text-xl mb-2 text-gray-lightbulma">
+              Recommend a dish
+            </p>
+            <p className=" text-gray-lightbulma ">
+              Get a recommendation for a delicious dish.
+            </p>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/2 mb-6 md:mb-0 p-3">
+          <div className="bg-white-10 rounded-lg shadow-md p-4">
+            <p className="font-semibold text-xl mb-2 text-gray-lightbulma">
+              Recommend a dish
+            </p>
+            <p className=" text-gray-lightbulma ">
+              Get a recommendation for a delicious dish.
+            </p>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/2 mb-6 md:mb-0 p-3">
+          <div className="bg-white-10 rounded-lg shadow-md p-4">
+            <p className="font-semibold text-xl mb-2 text-gray-lightbulma">
+              Recommend a dish
+            </p>
+            <p className=" text-gray-lightbulma ">
+              Get a recommendation for a delicious dish.
+            </p>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/2 mb-6 md:mb-0 p-3">
+          <div className="bg-white-10 rounded-lg shadow-md p-4">
+            <p className="font-semibold text-xl mb-2 text-gray-lightbulma">
+              Recommend a dish
+            </p>
+            <p className=" text-gray-lightbulma ">
+              Get a recommendation for a delicious dish.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default StaticSugestion;
