a9d9b7be209ca4ddc30ed8745e9410a79dd2edea
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-02-08 18:03:35 +0530

    fixses

--- modelfrontend/src/components/Feedback/FeedbackModal.js
+++ modelfrontend/src/components/Feedback/FeedbackModal.js
@@ -13,15 +13,15 @@ function FeedbackModal({ handleClose }) {
 
   return (
     <div className="fixed top-0 left-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50 md:inset-0 h-[calc(150%-1rem)] max-h-full">
-      <div className="bg-white-10 rounded-lg p-4 max-w-md w-1/4">
-          <h1 className="text-md font-bold mb-4">
-           👎 Provide additional feedback
-          </h1>
+      <div className="bg-white-10 rounded-lg p-4 max-w-md w-1/4 shadow-lg">
+        <h1 className="text-md font-bold mb-4">
+          👎 Provide additional feedback
+        </h1>
         <textarea
           value={feedback}
           onChange={(e) => setFeedback(e.target.value)}
           placeholder="What was the issue with the response? How could it be improved?"
-          className="p-2 h-36 w-full resize-none rounded-md shadow-md"
+          className="p-2 h-36 w-full resize-none rounded-md border border-gray-text shadow-md"
         />
         <div className="flex justify-end">
           <button
@@ -31,7 +31,7 @@ function FeedbackModal({ handleClose }) {
           </button>
           <button
             onClick={handleClose}
-            className="px-4 py-2 ml-4 bg-gray-text text-white rounded-md hover:bg-gray-500 text-white-10">
+            className="px-4 py-2 ml-4 bg-gray-lightbulma text-white rounded-md hover:bg-gray-text text-white-10">
             Close
           </button>
         </div>
